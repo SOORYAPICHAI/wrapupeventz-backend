ce277b3051fe1265e924ee99674efcf557800ad2
Your Name <jayasoorya@crayond.co>
2020-07-12 22:57:29 +0530

    Updated the index file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/test', function (req, res) {
   res.send('Hello Digital Ocean!')
 })
 
 app.listen(8080, function () {
-  console.log('Magic is happening on port 3000!')
+  console.log('Magic is happening on port 8080!')
 })
