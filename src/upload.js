50b64f07805770317eaf63ccc98b3259fcf43657
Your Name <jayasoorya@crayond.co>
2020-08-28 21:41:50 +0530

    updated the upload API

--- src/upload.js
+++ src/upload.js
@@ -1,15 +1,13 @@
-// const models = require('../models');
+const models = require("../models");
 // var uuid4 = require('uuid4');
 // const fs = require("fs");
 const aws = require("aws-sdk");
 var uuid4 = require("uuid4");
 
 module.exports = (req, res) => {
-  console.log(req);
-  console.log(req.files);
-  console.log(Object.keys(req.files));
-  console.log(Object.keys(req.files).length);
-  // console.log(req.files['file-upload']);
+  const created_at = new Date(),
+    updated_at = new Date();
+  const { answers } = models;
 
   const s3 = new aws.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -53,14 +51,13 @@ module.exports = (req, res) => {
         req.files[val].constructor === Array
           ? req.files[val]
           : [req.files[val]];
-      let calls = await _mapper.map(async (subVal,i) => {
-
+      let calls = await _mapper.map(async (subVal, i) => {
         console.log("subVal", subVal);
         var params = {
           Bucket: "wrapupeventzimages",
-          Key: `answers/profile_${
-            req.body.profile_id[0]
-          }/question_${val}-${subVal.name.split(".")[0]}.${subVal.name.split(".")[1]}`, // File name you want to save as in S3
+          Key: `answers/profile_${req.body.profile_id[0]}/question_${val}-${
+            subVal.name.split(".")[0]
+          }.${subVal.name.split(".")[1]}`, // File name you want to save as in S3
           Body: subVal.data,
           ACL: "public-read",
           ContentType: subVal.mimetype,
@@ -97,7 +94,7 @@ module.exports = (req, res) => {
   };
   const uploadMultipleImages = async () => {
     // Setting up S3 upload parameters
-
+    res.status(200).send({ message: "data saved successfully" });
     return new Promise(async (resolve, reject) => {
       console.log("here@@");
       let _keys = Object.keys(req.files);
@@ -107,10 +104,31 @@ module.exports = (req, res) => {
       });
       await Promise.all(calls)
         .then((data) => {
+          let _answers_array = JSON.parse(req.body.answers).answers;
+
+          let _profile_id = JSON.parse(req.body.answers).profile_id;
           console.log(data);
-          res
-            .status(200)
-            .send({ message: "uploaded successfully", data: data });
+          data.map((val) => {
+            var _obj = {};
+            var keys = Object.keys(val)[0];
+            var values = Object.values(val)[0].map((val) => {
+              return val.Location;
+            });
+            _obj["id"] = keys;
+            _obj["answer"] = values.toString();
+            _answers_array.push(_obj);
+          });
+
+          // res
+          //   .status(200)
+          //   .send({ message: "uploaded successfully", data: data });
+          _uploadQuestions(_answers_array, _profile_id)
+            .then((val) => {
+              res
+                .status(200)
+                .send({ message: "data saved successfully", data: val });
+            })
+            .catch(() => {});
           resolve(true);
         })
         .catch((err) => {
@@ -124,6 +142,34 @@ module.exports = (req, res) => {
 
   // });
 
+  //  SAVE QUESTIONS
+
+  const _uploadQuestions = async (data, _profile_id) => {
+    return new Promise(async (resolve, reject) => {
+      let calls = data.map(async (val) => {
+        console.log("---------------", val);
+        let idUUID = uuid4();
+        const __answers = {
+          question_id: val.id,
+          answer: val.answer,
+          _id: idUUID,
+          profile_id: _profile_id,
+          createdAt: created_at,
+          updatedAt: updated_at,
+        };
+        return await answers.create(__answers);
+      });
+      await Promise.all(calls)
+        .then((data) => {
+          resolve(true);
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(false);
+        });
+    });
+  };
+
   try {
     const profileUpload = async () => {
       let _keys = Object.keys(req.files);
@@ -133,6 +179,8 @@ module.exports = (req, res) => {
       } else {
         console.log("<><><><><><><>M<>");
         await uploadMultipleImages();
+        console.clear();
+        // console.log(_answers,"_answers_answers_answers_answers");
       }
     };
     profileUpload();
